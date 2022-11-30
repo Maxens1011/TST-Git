d429ad839efa8c5a955d21741559002ee5605b62
Jujuazer <justinprat07@gmail.com>
2022-11-30 16:31:27 +0100

    start

---
+++ SpaceShooterSFML/SpaceShooterSFML.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+	// Initialise everything below
+	// Game loop
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			// Process any input event here
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		// Whatever I want to draw goes here
+		window.display();
+	}
+}
